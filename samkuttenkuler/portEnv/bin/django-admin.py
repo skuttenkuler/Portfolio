7cef1b38409df1bbf8601ab5f3aced9616f2bb95
djsamkidd <33381106+djsamkidd@users.noreply.github.com>
2019-07-29 20:03:36 -0700

    changes2

---
+++ samkuttenkuler/portEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/samkuttenkuler/Desktop/Dev/Projects/Finished Projects/samkuttenkuler/samkuttenkuler/portEnv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
